6dcf9a22256e7e27a18b733af9f8a2ed111a3353
MAAZQAZI <81867779+MAAZQAZI@users.noreply.github.com>
2021-04-25 18:24:40 +0500

    now

--- src/Task123/Task123.java
+++ src/Task123/Task123.java
@@ -163,7 +163,7 @@ static int puncCount(String c) {
         Task2 t2 = new Task2();
         Task3 t3 = new Task3();
         Task4 t4 = new Task4();
-        int a=0;
+        int a=1;
         Scanner scanner = new Scanner(System.in);
         String option;
         String input_String;
