2991ae10601149c4296d460a1b5380fb66463ddd
AqibChattha <81791938+AqibChattha@users.noreply.github.com>
2021-04-25 19:48:49 +0500

    Update Task123.java

--- src/Task123/Task123.java
+++ src/Task123/Task123.java
@@ -18,6 +18,7 @@ public class Task123 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int a = 96;
         Task1 t1 = new Task1();
         Task2 t2 = new Task2();
         Task3 t3 = new Task3();
